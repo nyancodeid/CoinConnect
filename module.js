eb76d678b6ae239a0ba8e5d781b2e8e23979032c
nyancodeid <ryandevstudio@gmail.com>
2018-01-14 16:47:01 +0700

    renamed

--- module.js
+++
@@ -1,9 +0,0 @@
-function isRunning() {
-	console.log("yeah it's running")
-}
-function isAlreadyRunning() {
-	console.log("yeah it's isAlreadyRunning")
-}
-function isTotalyRunning() {
-	console.log("yeah it's isTotalyRunning")
-}
